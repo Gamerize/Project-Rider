739d395586f26e28ac83397941cf6810a504b316
Gamerize <gundam.issacwai@gmail.com>
2022-04-08 15:49:33 +0100

    Added cinemachine

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float Speed = 10f;
+    private float rotationSpeed;
 
     // Update is called once per frame
     void Update()
     {
+        float horizontalmovement = Input.GetAxis("Horizontal");
+        float verticalmovement = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalmovement, 0, verticalmovement);
+        movementDirection.Normalize();
+
+        if(movementDirection != Vector3.zero)
+        {
+            transform.forward = movementDirection;
+        }
+
+        transform.Translate(movementDirection * rotationSpeed * Time.deltaTime);
+
         if (Input.GetKey(KeyCode.W) && !Input.GetMouseButton(0)) 
         {
             transform.Translate(0f, 0f, Time.deltaTime * Speed);
